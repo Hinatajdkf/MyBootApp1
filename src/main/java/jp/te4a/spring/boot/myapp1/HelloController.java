f6c5a218ca13a73017452abcd6b4ee287056aa56
武田 ひなた <210011@jc-21.jp>
2024-05-15 11:07:16 +0900

    更新

--- src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
@@ -1,23 +1,18 @@
 package jp.te4a.spring.boot.myapp1;
  
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
+import org.springframework.web.bind.annotation.RestController;
 /**
 * Hello world!
 *
 */
 @RestController
-public class HelloController 
-{
-	@RequestMapping("/")
-	public String index() {
-		return "Hello, Spring Boot!";
-	}
+public class HelloController {
 
-	@RequestMapping("/")
-	public String sato() {
-		return "Sato desu!";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "Hello, world!";
+    }
 	
 }
